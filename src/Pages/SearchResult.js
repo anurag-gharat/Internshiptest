e7d8ffe3a37f96b093ab07845fed810b82c6c717
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-04-30 16:48:58 +0530

    "search query result"

--- src/Pages/SearchResult.js
+++ src/Pages/SearchResult.js
@@ -4,15 +4,42 @@ export default class SearchResult extends Component {
     
     constructor(props){
         super(props)
-        console.log(props.location.state.data)
+        this.state={
+            title:props.location.state.data,
+            description:"Some description",
+
+        }
+        // console.log(props.location.state.data)
     }
 
     render() {
 
         return (
             <div>
-                <div className="container mx-auto mt-5 bg-dark">
-                    asdasd
+                <div className="container mx-auto p-4 mt-5 bg-white shadow-lg">
+                    <div className="text-center">
+                        <h1 className="font-weight-bold">
+                            How can you learn externally
+                        </h1>
+                        <span className="text-decoration-underline"><u><h1  className="font-weight-bold">"{this.state.title}"</h1></u></span>
+                        <h1  className="font-weight-bold">meanwhile we are coming with distinct approach?</h1>
+                    </div>
+                    <div className="mt-5">
+                        <h3>The unique platform of Edunomics</h3>
+                        <p className="font-weight-lighter">
+                        This one of a kind platform is currently in construction, being devised with the most effectual and productive learning ecosystem for especially those students who have the urge to firstly find their deep hidden talents which they can excel with. And secondly for those curious souls who don't quite believe in the concept of learning merely by THEORETICAL ways. Who feel that a learning process should be stressed upon the PRACTICAL and real life scenarios because the things you grasp by such a realistic study, gets embedded into your memory with a profound clarity that no other teaching method can give you. Agreed? You surely must have felt this in your school and college days.
+                        That is exactly what we are trying to offer students here at Edunomics, building a bridge between industry and academics. Since we are aware of the fact that a recorded descriptive video lecture or a written PDF'd notes or PPT can surely give you knowledge on the subject, but it's human psychology to start fast forwarding and glancing through it once you clasp the basic idea of what the topic is about. And yeah sometimes it can be a bit of a bore, right?
+                        So how about an interactive and interesting (with a little bit of fun sprinkled) live video sessions by industry experts who will focus 30% on academics to lay your basic foundations strong and remaining 70% to practically train you with the latest up to date industry trends. Sounds compelling enough? 
+                        </p>
+                    </div>
+                    <div>
+                    <h3>How can you prep up, till we are set to launch!</h3>
+                        <p className="font-weight-lighter">
+                        Now, for those of you who wanna be a little ahead of the curve and utilise this short period of time prior to our kick off, this is what we suggest for you so that you can have a clear picture on what your target field will be at Edunomics.
+                        Let's say this is a part of the 30% you can have to start with a lead when you enroll with our co active programs. One platform that we recommend you can access and browse through different areas and specializations to really understand what you want to pursue is Udemy. It won't be long before we arise with a fully distinguished and gratified product that we have initiated, but in the meantime we suggest you check out the following by clicking here. This programmed module will give you a list of courses that you can go through and find what particularly interests you. Because we are not a market selfish entity but instead aim to assist and nurture you right from the basics to a full fledged professional with your own uniqueness! Nevertheless, you can check this out too, for a more deeper research on what course will benefit you the best.
+                        Hope that will be beneficial and any remaining doubts you can contact us, our professional team aspires to help those in need!
+                        </p>
+                    </div>
                 </div>
             </div>
         )
